af3d94a9ba50c657934f0f1e3862a27be5df3b2b
Hecti1008 <59198132+Hecti1008@users.noreply.github.com>
2021-05-02 14:27:58 +0200

    Add files via upload

--- src/ac10_uf3_m9/client.java
+++ src/ac10_uf3_m9/client.java
@@ -22,88 +22,89 @@ public class client implements Runnable {
     /**
      * @param args the command line arguments
      */
-    
     private static Socket client;
     private static boolean follow = true;
-    
+
     public client(Socket client) {
         this.client = client;
     }
-        public static void main (String[] args) throws Exception {
-		
-		String host = "localhost";
-		int port = 60000;//Port remot
-		client = new Socket(host, port);
-		
-		//FLUX fDE SORTIDA AL SERVIDOR
-		PrintWriter fsortida = new PrintWriter(client.getOutputStream(), true);
-		
-		//FLUX D'ENTRADA AL SERVIDOR
-		BufferedReader fentrada = new BufferedReader(new InputStreamReader(client.getInputStream()));
-		
-		//FLUX PER A ENTRADA ESTÀNDARD
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		
-		String name = "";
-		System.out.println("Introdueix el teu nom: ");
-		//Lectura teclat
-		name = in.readLine();
-		
-		while (name != null || name.equals("")) {
-			
-                    System.out.println("Error al introduir el nom, torna a intentar: ");
-                    name = in.readLine();
-			
-		}
-                
-                Runnable run = new client(client);
-                Thread clientEnter = new Thread(run);
-                
-                String cadena, eco = "";
-                System.out.println("Missatge pel chat: ");
-                cadena = in.readLine();
-                
-                while (cadena != null && (!cadena.equals("(exit)"))) {
 
-                            fsortida.println(name + ": " + cadena);
-                            eco = fentrada.readLine();
-                            System.out.println(" =>Eco: "+eco);
-                            cadena = in.readLine();
-                            
-                            if (cadena == null || cadena.equals("")) {
-                                fsortida.print(cadena);
-                            }
-                    }
-                
-                notFollow();
-                fsortida.close();
-		fentrada.close();
-		System.out.println("Finalització de l'enviament...");
-		in.close();
-		client.close();
+    public static void main(String[] args) throws Exception {
+
+        String host = "localhost";
+        int port = 60000;//Port remot
+        client = new Socket(host, port);
+
+        //FLUX fDE SORTIDA AL SERVIDOR
+        PrintWriter fsortida = new PrintWriter(client.getOutputStream(), true);
+
+        //FLUX D'ENTRADA AL SERVIDOR
+        BufferedReader fentrada = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+        //FLUX PER A ENTRADA ESTÀNDARD
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        String name = "";
+        System.out.println("Introdueix el teu nom: ");
+        //Lectura teclat
+        name = in.readLine();
+
+        while (name != null || name.equals("")) {
+
+            System.out.println("Error al introduir el nom, torna a intentar: ");
+            name = in.readLine();
+
         }
-                public static void notFollow() {
-                    follow = false;
-                }
-                
-                @Override
-                public void run() {
-                    while (follow == true) {
-                      try {
-                          
-                BufferedReader fentrada = new BufferedReader (new InputStreamReader(client.getInputStream()));
+
+        Runnable run = new client(client);
+        Thread clientEnter = new Thread(run);
+
+        String cadena, eco = "";
+        System.out.println("Missatge pel chat: ");
+        cadena = in.readLine();
+
+        while (cadena != null && (!cadena.equals("(exit)"))) {
+
+            fsortida.println(name + ": " + cadena);
+            eco = fentrada.readLine();
+            System.out.println(" =>Eco: " + eco);
+            cadena = in.readLine();
+
+            if (cadena == null || cadena.equals("")) {
+                fsortida.print(cadena);
+            }
+        }
+
+        notFollow();
+        fsortida.close();
+        fentrada.close();
+        System.out.println("Finalització de l'enviament...");
+        in.close();
+        client.close();
+    }
+
+    public static void notFollow() {
+        follow = false;
+    }
+
+    @Override
+    public void run() {
+        while (follow == true) {
+            try {
+
+                BufferedReader fentrada = new BufferedReader(new InputStreamReader(client.getInputStream()));
                 String enter = fentrada.readLine();
-                
-                if(enter == null || enter.equals("(exit)") || enter.equals("null")) {
+
+                if (enter == null || enter.equals("(exit)") || enter.equals("null")) {
                     notFollow();
-                }else{
+                } else {
                     System.out.println(enter);
                 }
-            }catch(SocketException e){
-                
-            }catch(IOException e) {
+            } catch (SocketException e) {
+
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-        }  
-                    }
-                }
+        }
+    }
+}
